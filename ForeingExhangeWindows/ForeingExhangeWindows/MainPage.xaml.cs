b0db6d0eb40316f15dcb46a368faa8bd162fb2ff
CAROLINA CASTA�O GONZALEZ <carolinacastano90799@correo.itm.edu.co>
2017-08-08 21:02:18 -0500

    App Demo

---
+++ ForeingExhangeWindows/ForeingExhangeWindows/MainPage.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace ForeingExhangeWindows
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+
+            ConvertButton.Clicked += ConvertButton_Clicked;
+        }
+        private void ConvertButton_Clicked(object sender, EventArgs e)
+        {
+            if (string.IsNullOrEmpty(PesosEntry.Text))
+            {
+                DisplayAlert("Error", "You must enter a value in pesos.", "Accept");
+                return;
+            }
+
+            var pesos = decimal.Parse(PesosEntry.Text);
+
+            var dollars = pesos / 2994.01198M;
+            var euros = pesos / 3518.11377M;
+            var pounds = pesos / 3890.41916M;
+
+            DollarsEntry.Text = string.Format("{0:C2}", dollars);
+            EurosEntry.Text = string.Format("{0:C2}", euros);
+            PoundsEntry.Text = string.Format("{0:C2}", pounds);
+        }
+    }
+}
